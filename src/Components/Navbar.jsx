6d5b265500c3fefc3865761b1474ea7a91a65432
devwo1f <48999616+devwo1f@users.noreply.github.com>
2021-09-25 16:39:37 +0530

    added page-specific-navbar (needs more customization)

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,44 @@
+import { Navbar, Container, Nav, NavDropdown } from "react-bootstrap";
+import logosmall from "../Assets/ey-small.png";
+
+function Navbarcommon() {
+  return (
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+      <Container>
+        <Navbar.Brand href="#home">
+          <img
+            src={logosmall}
+            width="30"
+            height="35"
+            className="d-inline-block align-top"
+            alt="ey-small-logo"
+          />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+          <Nav>
+            <Nav.Link href="/register">Register</Nav.Link>
+            <Nav.Link href="/login">Login</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default Navbarcommon;
