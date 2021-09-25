ad48134d97313097bddb77446a65f483671eb950
devwo1f <48999616+devwo1f@users.noreply.github.com>
2021-09-25 20:32:16 +0530

    navbar changes

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -12,25 +12,14 @@ function Navbarcommon() {
             height="35"
             className="d-inline-block align-top"
             alt="ey-small-logo"
-          />
+          />{" "}
+          <span className="brand-name">
+            <span className="bold-brand-name">WOLVES</span> SEARCH
+          </span>
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
-            <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
+          <Nav className="me-auto"></Nav>
           <Nav>
             <Nav.Link href="/register">Register</Nav.Link>
             <Nav.Link href="/login">Login</Nav.Link>
