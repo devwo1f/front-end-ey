22176f566b59d735c1d37c48a7aaf2f5512984c0
devwo1f <48999616+devwo1f@users.noreply.github.com>
2021-09-27 02:40:13 +0530

    Registration form validation for password added

--- src/Components/Register.jsx
+++ src/Components/Register.jsx
@@ -1,10 +1,63 @@
+import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import { Button, Form, Col } from "react-bootstrap";
 import logo from "../Assets/ey-logo.png";
 import "../css/register.css";
 import Navbarcommon from "./Navbar";
 
+const initialState = {
+  firstname: "",
+  lastname: "",
+  email: "",
+  password: "",
+  confirmpassword: "",
+};
+
+const passVerificationError = {
+  isLengthy: false,
+  hasUpper: false,
+  hasLower: false,
+  hasSpclChar: false,
+  hasNumber: false,
+  confirmPassword: false,
+};
+
 function Register() {
+  const [newUser, setNewUser] = useState(initialState);
+  const [passwordError, setPasswordError] = useState(passVerificationError);
+
+  useEffect(() => {}, [newUser]);
+
+  const handleOnChange = (e) => {
+    const { name, value } = e.target;
+
+    setNewUser({ ...newUser, [name]: value });
+
+    if (name === "confirmpassword") {
+      setPasswordError({
+        ...passwordError,
+        confirmPassword: newUser.password === value,
+      });
+    }
+
+    if (name === "password") {
+      const isLengthy = value.length > 8;
+      const hasUpper = /[A-Z]/.test(value);
+      const hasLower = /[a-z]/.test(value);
+      const hasNumber = /[0-9]/.test(value);
+      const hasSpclChar = /[@,!,#,%,&]/.test(value);
+
+      setPasswordError({
+        ...passwordError,
+        isLengthy,
+        hasLower,
+        hasNumber,
+        hasSpclChar,
+        hasUpper,
+      });
+    }
+  };
+  console.log(newUser);
   return (
     <div>
       <Navbarcommon />
@@ -12,29 +65,108 @@ function Register() {
         <Form style={{ display: "grid", justifyContent: "center" }}>
           <img className="logo" src={logo} alt="ey-logo" />
           <Row className="mb-3">
-            <Form.Group as={Col} controlId="formGridFirstName">
-              <Form.Control type="email" placeholder="First Name" />
+            <Form.Group as={Col}>
+              <Form.Control
+                type="text"
+                name="firstname"
+                onChange={handleOnChange}
+                placeholder="First Name"
+                value={newUser.firstname}
+              />
             </Form.Group>
 
-            <Form.Group as={Col} controlId="formGridMiddleName">
-              <Form.Control type="email" placeholder="Last Name" />
+            <Form.Group as={Col}>
+              <Form.Control
+                type="text"
+                name="lastname"
+                placeholder="Last Name"
+                onChange={handleOnChange}
+                value={newUser.lastname}
+              />
             </Form.Group>
           </Row>
 
-          <Form.Group className="mb-3" controlId="formBasicEmail">
-            <Form.Control type="email" placeholder="Enter email" />
+          <Form.Group className="mb-3">
+            <Form.Control
+              type="email"
+              name="email"
+              placeholder="Enter email"
+              onChange={handleOnChange}
+              value={newUser.email}
+            />
             <Form.Text className="text-muted">
               We'll never share your email with anyone else.
             </Form.Text>
           </Form.Group>
+          <Form.Group className="mb-3">
+            <Form.Control
+              type="text"
+              name="department"
+              placeholder="Department Name"
+            />
+          </Form.Group>
 
-          <Form.Group className="mb-3" controlId="formBasicPassword">
-            <Form.Control type="password" placeholder="Password" />
+          <Form.Group className="mb-3">
+            <Form.Control
+              type="password"
+              name="password"
+              placeholder="Password"
+              onChange={handleOnChange}
+              value={newUser.password}
+            />
           </Form.Group>
-          <Form.Group className="mb-3" controlId="formBasicPassword">
-            <Form.Control type="password" placeholder="Confirm Password" />
+          <Form.Group className="mb-3">
+            <Form.Control
+              type="password"
+              name="confirmpassword"
+              placeholder="Confirm Password"
+              onChange={handleOnChange}
+              value={newUser.confirmpassword}
+            />
           </Form.Group>
-          <Button variant="primary" type="submit">
+
+          <ul className="mb-4">
+            <li
+              className={
+                passwordError.isLengthy ? "text-success" : "text-danger"
+              }
+            >
+              Min 8 characters
+            </li>
+            <li
+              className={
+                passwordError.hasUpper ? "text-success" : "text-danger"
+              }
+            >
+              At least One upper case
+            </li>
+            <li
+              className={
+                passwordError.hasLower ? "text-success" : "text-danger"
+              }
+            >
+              At least one lower case
+            </li>
+            <li
+              className={
+                passwordError.hasNumber ? "text-success" : "text-danger"
+              }
+            >
+              At least one number
+            </li>
+            <li
+              className={
+                passwordError.hasSpclChar ? "text-success" : "text-danger"
+              }
+            >
+              At least one special character
+            </li>
+          </ul>
+          <Button
+            variant="primary"
+            type="submit"
+            disabled={Object.values(passwordError).includes(false)}
+          >
             Register
           </Button>
         </Form>
