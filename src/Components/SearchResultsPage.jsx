9fc1ac8f1e139ab60331c1a2217d8b90bd7eecad
devwo1f <48999616+devwo1f@users.noreply.github.com>
2021-09-27 23:03:25 +0530

    Component Bifurcation

--- src/Components/SearchResultsPage.jsx
+++ src/Components/SearchResultsPage.jsx
@@ -9,6 +9,9 @@ import {
 } from "react-bootstrap";
 import "../css/sidebar.css";
 import NavbarCommonAcross from "./NavbarCommonAcross";
+import SearchResultsBox from "./SearchResultsBox";
+import SidebarFilters from "./SidebarFilters";
+import TagsSearchResultsBox from "./TagsSearchResultsBox";
 
 function SearchResultsPage() {
   return (
@@ -18,65 +21,14 @@ function SearchResultsPage() {
         <Row>
           <Col sm={2}>
             <br />
-            <div className="sidebar-div">
-              <h2>Filters</h2>
-              <Form.Label>
-                <span className="filter-headings">Type:</span>
-              </Form.Label>
-              <Form.Check label="PDF" />
-              <Form.Check label="Video" />
-              <Form.Check label="Document" />
-              <Form.Check label="Image" />
-              <br />
-              <Form.Label>
-                <span className="filter-headings">Department:</span>
-              </Form.Label>
-              <Form.Select aria-label="Default select example">
-                <option>Department</option>
-                <option value="1">Engineering</option>
-                <option value="2">Accounts</option>
-                <option value="3">Finance</option>
-                <option value="3">Public Relations</option>
-              </Form.Select>
-              <br />
-              <Form.Label>
-                <span className="filter-headings">Additional Filters:</span>
-              </Form.Label>
-              <Form.Check label="Uploaded By me" />
-              <Form.Check label="Only Objects" />
-              <Form.Check label="Only Text" />
-            </div>
+            <SidebarFilters />
           </Col>
           <Col sm={10}>
             <br />
             <div className="search-results-page">
               <h2>Search results...</h2>
-              <Badge bg="warning" text="dark">
-                PDF <CloseButton className="closebutton-tags" />
-              </Badge>{" "}
-              <Badge bg="warning" text="dark">
-                Engineering <CloseButton className="closebutton-tags" />
-              </Badge>{" "}
-              <Badge bg="warning" text="dark">
-                Uploaded by me <CloseButton className="closebutton-tags" />
-              </Badge>{" "}
-              <div className="results">
-                <Row xs={2} md={3} className="g-4">
-                  {Array.from({ length: 20 }).map((_, idx) => (
-                    <Col>
-                      <Card>
-                        <Card.Img
-                          variant="top"
-                          src="https://source.unsplash.com/random/800x800"
-                        />
-                        <Card.Body>
-                          <Card.Title>Image</Card.Title>
-                        </Card.Body>
-                      </Card>
-                    </Col>
-                  ))}
-                </Row>
-              </div>
+              <TagsSearchResultsBox />
+              <SearchResultsBox />
             </div>
           </Col>
         </Row>
