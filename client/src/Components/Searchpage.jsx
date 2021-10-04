148a7231e41f2bfc092d3fe31d358bf6f4c2e37f
devwo1f <48999616+devwo1f@users.noreply.github.com>
2021-10-05 00:32:28 +0530

    ch

---
+++ client/src/Components/Searchpage.jsx
@@ -0,0 +1,31 @@
+import "../css/searchpage.css";
+import logo1 from "../Assets/ey-logo.png";
+import { InputGroup, FormControl, Button, Form } from "react-bootstrap";
+import Filtermodal from "./Filtermodal";
+import NavbarSearchPage from "./NavbarSearchPage";
+
+function Searchpage() {
+  return (
+    <div>
+      <NavbarSearchPage />
+      <div className="form">
+        <img src={logo1} className="logo22" alt="EY_LOGO" />
+        <InputGroup className="mb-3 form-search">
+          <FormControl
+            placeholder="Search!"
+            aria-label="Search!"
+            aria-describedby="basic-addon2"
+          />
+        </InputGroup>
+        <div className="buttons">
+          <Filtermodal />{" "}
+          <Button variant="secondary" className="broadsearch-button" active>
+            Broad Search
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Searchpage;
