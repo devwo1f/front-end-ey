f8128df18cbf1f208a0a30201bae12182fd547ed
devwo1f <48999616+devwo1f@users.noreply.github.com>
2021-10-06 00:59:11 +0530

    updates

--- client/src/Components/Searchpage.jsx
+++ client/src/Components/Searchpage.jsx
@@ -1,8 +1,12 @@
+import { useForm } from "react-hook-form";
+import fs from "fs";
+import axios from "axios";
 import "../css/searchpage.css";
 import logo1 from "../Assets/ey-logo.png";
 import { InputGroup, FormControl, Button, Form } from "react-bootstrap";
 import Filtermodal from "./Filtermodal";
 import NavbarSearchPage from "./NavbarSearchPage";
+import UploadModal from "./UploadModal";
 
 function Searchpage() {
   return (
@@ -17,11 +21,16 @@ function Searchpage() {
             aria-describedby="basic-addon2"
           />
         </InputGroup>
+        <form>
+          <input type="file" name="picture" />
+          <button>Submit</button>
+        </form>
         <div className="buttons">
           <Filtermodal />{" "}
           <Button variant="secondary" className="broadsearch-button" active>
             Broad Search
           </Button>
+          <UploadModal />
         </div>
       </div>
     </div>
