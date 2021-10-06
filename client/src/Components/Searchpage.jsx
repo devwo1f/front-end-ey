22a0e84d3c57b065b4c082850b75f707488c9eee
devwo1f <48999616+devwo1f@users.noreply.github.com>
2021-10-06 23:37:24 +0530

    ch

--- client/src/Components/Searchpage.jsx
+++ client/src/Components/Searchpage.jsx
@@ -1,5 +1,4 @@
 import { useForm } from "react-hook-form";
-import fs from "fs";
 import axios from "axios";
 import "../css/searchpage.css";
 import logo1 from "../Assets/ey-logo.png";
@@ -7,8 +6,33 @@ import { InputGroup, FormControl, Button, Form } from "react-bootstrap";
 import Filtermodal from "./Filtermodal";
 import NavbarSearchPage from "./NavbarSearchPage";
 import UploadModal from "./UploadModal";
+import FormData from "form-data";
+import { useState } from "react";
 
 function Searchpage() {
+  const [file, setFile] = useState();
+  var formdata = new FormData();
+  var myHeaders = new Headers();
+  const onFileChange = (e) => setFile(e.target.files[0]);
+  formdata.append("image", file);
+  myHeaders.append(
+    "Cookie",
+    "ARRAffinity=184b93d9bd44b3d34ffbdb54e8c0a88f58a67edb02b538a38e803ce6c23758f2; ARRAffinitySameSite=184b93d9bd44b3d34ffbdb54e8c0a88f58a67edb02b538a38e803ce6c23758f2"
+  );
+
+  var requestOptions = {
+    method: "POST",
+    body: formdata,
+    redirect: "follow",
+  };
+
+  function fetchApi() {
+    fetch("https://eyyolo101.azurewebsites.net/appi", requestOptions)
+      .then((response) => response.text())
+      .then((result) => console.log(result))
+      .catch((error) => console.log("error", error));
+  }
+
   return (
     <div>
       <NavbarSearchPage />
@@ -22,8 +46,10 @@ function Searchpage() {
           />
         </InputGroup>
         <form>
-          <input type="file" name="picture" />
-          <button>Submit</button>
+          <input type="file" onChange={onFileChange} name="picture" />
+          <button onClick={fetchApi} type="button">
+            Submit
+          </button>
         </form>
         <div className="buttons">
           <Filtermodal />{" "}
