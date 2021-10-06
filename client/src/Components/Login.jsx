599d8575ee02e202db4e7a61db632fb0d43b13bb
devwo1f <48999616+devwo1f@users.noreply.github.com>
2021-10-06 23:37:16 +0530

    ch

--- client/src/Components/Login.jsx
+++ client/src/Components/Login.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Col, Container, Row } from "react-bootstrap";
 import { Button, Form } from "react-bootstrap";
 import logo from "../Assets/ey-logo.png";
@@ -5,25 +6,62 @@ import "../css/login.css";
 import Navbarcommon from "./Navbar";
 
 function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  async function loginUser(event) {
+    event.preventDefault();
+
+    const response = await fetch("http://localhost:1337/api/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email,
+        password,
+      }),
+    });
+
+    const data = await response.json();
+
+    if (data.user) {
+      localStorage.setItem("token", data.user);
+      alert("Login successful");
+      window.location.href = "/dashboard";
+    } else {
+      alert("Please check your username and password");
+    }
+  }
   return (
     <div>
       <Navbarcommon />
       <Container className="login-form">
-        <Form style={{ display: "grid", justifyContent: "center" }}>
+        <Form
+          style={{ display: "grid", justifyContent: "center" }}
+          onSubmit={loginUser}
+        >
           <img className="logo" src={logo} alt="ey-logo" />
           <Form.Group className="mb-3" controlId="formBasicEmail">
-            <Form.Control type="email" placeholder="Enter email" />
+            <Form.Control
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              type="email"
+              placeholder="Email"
+            />
             <Form.Text className="text-muted">
               We'll never share your email with anyone else.
             </Form.Text>
           </Form.Group>
           <Form.Group className="mb-3" controlId="formBasicPassword">
-            <Form.Control type="password" placeholder="Password" />
+            <Form.Control
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              type="password"
+              placeholder="Password"
+            />
           </Form.Group>
-          <Form.Group className="mb-3" controlId="formBasicCheckbox">
-            <Form.Check type="checkbox cb" label="Keep me signed in" />
-          </Form.Group>
-          <Button variant="primary" type="submit">
+          <Button variant="primary" type="submit" value="login">
             Login
           </Button>
         </Form>
