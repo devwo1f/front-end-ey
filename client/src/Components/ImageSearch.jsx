71531a2612ab253b3fe9dc163adf5d9eb1dfa6be
devwo1f <48999616+devwo1f@users.noreply.github.com>
2021-10-07 19:58:35 +0530

    update UI

---
+++ client/src/Components/ImageSearch.jsx
@@ -0,0 +1,39 @@
+import { Form, Button } from "react-bootstrap";
+import FormData from "form-data";
+import { useState } from "react";
+
+function ImageSearch() {
+  var formdata = new FormData();
+  const [file, setFile] = useState();
+  formdata.append("image", file);
+  const onFileChange = (e) => setFile(e.target.files[0]);
+  var requestOptions = {
+    method: "POST",
+    body: formdata,
+    redirect: "follow",
+  };
+  function handleClick() {
+    fetch("https://eyyolo101.azurewebsites.net/appi", requestOptions)
+      .then((response) => response.text())
+      .then((result) => console.log(result))
+      .catch((error) => console.log("error", error));
+  }
+  //   function handleClick() {
+  //     fetch("https://api.kanye.rest")
+  //       .then((response) => response.text())
+  //       .then((result) => console.log(result))
+  //       .catch((error) => console.log("error", error));
+  //   }
+  return (
+    <form>
+      <Form.Group className="mb-3">
+        <Form.Control onChange={onFileChange} type="file" />
+      </Form.Group>
+      <Button onClick={handleClick} type="button">
+        Search!
+      </Button>
+    </form>
+  );
+}
+
+export default ImageSearch;
