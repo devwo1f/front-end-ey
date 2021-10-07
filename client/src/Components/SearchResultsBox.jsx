1df06b022b0cf42a8767f373c05aac8467146432
devwo1f <48999616+devwo1f@users.noreply.github.com>
2021-10-07 19:58:48 +0530

    update UI

--- client/src/Components/SearchResultsBox.jsx
+++ client/src/Components/SearchResultsBox.jsx
@@ -1,24 +1,39 @@
-import { Row, Col, Badge, CloseButton, Card } from "react-bootstrap";
+import { Card, Row, Col } from "react-bootstrap";
+import "../css/resultbox.css";
 
 function SearchResultsBox() {
+  const array = [
+    "abhay_1633553219521",
+    "abhay_1633553245079",
+    "abhay_1633609356186",
+    "abhay_1633609364441",
+    "abhay_1633609371718",
+    "abhay_1633609379696",
+    "abhay_1633609403844",
+    "abhay_1633609418418",
+    "abhay_1633609428032",
+    "abhay_1633609435970",
+    "abhay_1633609444222",
+  ];
   return (
-    <div className="results">
-      <Row xs={2} md={3} className="g-4">
-        {Array.from({ length: 20 }).map((_, idx) => (
-          <Col>
-            <Card>
+    <Row xs={2} md={3} className="g-4">
+      {array.map((image) => (
+        <Col>
+          <Card>
+            <a
+              target="_blank"
+              href={`https://feblob.blob.core.windows.net/files/${image}`}
+            >
               <Card.Img
-                variant="top"
-                src="https://feblob.blob.core.windows.net/files/Abdul Ahad - Photo.jpg"
+                varian="top"
+                className="images-result"
+                src={`https://feblob.blob.core.windows.net/files/${image}`}
               />
-              <Card.Body>
-                <Card.Title>Image</Card.Title>
-              </Card.Body>
-            </Card>
-          </Col>
-        ))}
-      </Row>
-    </div>
+            </a>
+          </Card>
+        </Col>
+      ))}
+    </Row>
   );
 }
 
