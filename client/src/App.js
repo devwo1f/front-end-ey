b5497b189f39d7ecb0358396ded5fa2ef07fcb19
devwo1f <48999616+devwo1f@users.noreply.github.com>
2021-10-11 01:27:09 +0530

    updates

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useMemo, useState } from "react";
 import "./App.css";
 import Login from "./Components/Login";
 import Register from "./Components/Register";
@@ -8,17 +8,22 @@ import Loggedin from "./Components/Loggedin";
 import SearchResultsPage from "./Components/SearchResultsPage";
 import UserGetin from "./Components/UserGetin";
 import Upload from "./Components/Upload";
+import { UserContext } from "./UserContext";
 
 function App() {
+  const [value, setValue] = useState("user");
+  const providerValue = useMemo(() => ({ value, setValue }), [value, setValue]);
   return (
     <Router>
-      <Route path="/login" component={Login} />
-      <Route path="/register" component={Register} />
-      <Route path="/loggedin" component={Loggedin} />
-      <Route path="/searchpage" component={Searchpage} />
-      <Route path="/search" component={SearchResultsPage} />
-      <Route exact path="/" component={UserGetin} />
-      <Route path="/upload" component={Upload} />
+      <UserContext.Provider value={providerValue}>
+        <Route path="/login" component={Login} />
+        <Route path="/register" component={Register} />
+        <Route path="/loggedin" component={Loggedin} />
+        <Route path="/searchpage" component={Searchpage} />
+        <Route path="/search" component={SearchResultsPage} />
+        <Route exact path="/" component={UserGetin} />
+        <Route path="/upload" component={Upload} />
+      </UserContext.Provider>
     </Router>
   );
 }
