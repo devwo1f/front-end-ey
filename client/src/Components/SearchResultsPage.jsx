24d0bfef5e261318a16567a3d2882c2362ed5747
devwo1f <48999616+devwo1f@users.noreply.github.com>
2021-10-11 10:10:56 +0530

    MU

--- client/src/Components/SearchResultsPage.jsx
+++ client/src/Components/SearchResultsPage.jsx
@@ -1,11 +1,37 @@
-import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import React, { createContext, useState } from "react";
+import {
+  Container,
+  Row,
+  Col,
+  Form,
+  InputGroup,
+  FormControl,
+  Button,
+  Card,
+} from "react-bootstrap";
 import "../css/sidebar.css";
 import NavbarCommonAcross from "./NavbarCommonAcross";
-import SearchResultsBox from "./SearchResultsBox";
-import SidebarFilters from "./SidebarFilters";
 
 function SearchResultsPage() {
+  const [video, setVideo] = useState(false);
+  const [document, setDocument] = useState(false);
+  const [location, setLocation] = useState(false);
+  const [department, setDepartment] = useState(false);
+  const [uploadedByMe, setUploadedByMe] = useState(false);
+  const [text, setText] = useState("");
+  const [array, setArray] = useState([]);
+  const ResultArr = createContext();
+  function toggle(value) {
+    return !value;
+  }
+  async function searchFun() {
+    const response = await fetch(
+      `http://searchapi102.azurewebsites.net/searchtxt?usr_nm=abd1&upload_me=${uploadedByMe}&loc=${location}&dep=${department}&doc=${document}&vid=${video}&txt_q=${text}`
+    );
+    const data = await response.json();
+    console.log(data);
+    setArray(data);
+  }
   return (
     <div>
       <NavbarCommonAcross />
@@ -13,13 +39,74 @@ function SearchResultsPage() {
         <Row>
           <Col sm={2}>
             <br />
-            <SidebarFilters />
+            {/* Side Bars */}
+            <div className="sidebar-div">
+              <h2>Filters</h2>
+              <Form.Label>
+                <span className="filter-headings">Type:</span>
+              </Form.Label>
+              <Form.Label>
+                <span className="filter-headings">Type:</span>
+              </Form.Label>
+              <Form.Check onChange={() => setVideo(toggle)} label="Video" />
+              <Form.Check
+                onChange={() => setDocument(toggle)}
+                label="Document"
+              />
+              <br />
+              <Form.Label>
+                <span className="filter-headings">Additional Filters:</span>
+              </Form.Label>
+              <Form.Check
+                onChange={() => setLocation(toggle)}
+                label="My Location"
+              />
+              <Form.Check
+                onChange={() => setDepartment(toggle)}
+                label="My Department"
+              />
+              <Form.Check
+                onChange={() => setUploadedByMe(toggle)}
+                label="Uploaded By Me"
+              />
+            </div>
           </Col>
           <Col sm={10}>
             <br />
+            <InputGroup className="mb-3">
+              <FormControl
+                placeholder="Search"
+                aria-describedby="basic-addon2"
+                onChange={(e) => setText(e.target.value)}
+              />
+              <Button
+                variant="outline-warning"
+                onClick={searchFun}
+                id="button-addon2"
+              >
+                Search
+              </Button>
+            </InputGroup>
             <div className="search-results-page">
               <h2>Search results...</h2>
-              <SearchResultsBox />
+              <Row xs={2} md={3} className="g-4">
+                {array.map((image) => (
+                  <Col>
+                    <Card>
+                      <a
+                        target="_blank"
+                        href={`https://feblob.blob.core.windows.net/files/${image}`}
+                      >
+                        <Card.Img
+                          varian="top"
+                          className="images-result"
+                          src={`https://feblob.blob.core.windows.net/files/${image}`}
+                        />
+                      </a>
+                    </Card>
+                  </Col>
+                ))}
+              </Row>
             </div>
           </Col>
         </Row>
