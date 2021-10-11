647e1f2a538add24a2a0e1748db458e92351d628
devwo1f <48999616+devwo1f@users.noreply.github.com>
2021-10-11 12:08:24 +0530

    MU

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,28 @@
-import React, { useMemo, useState } from "react";
+import React, { createContext, useMemo, useState } from "react";
 import "./App.css";
 import Login from "./Components/Login";
 import Register from "./Components/Register";
 import Searchpage from "./Components/Searchpage";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import Loggedin from "./Components/Loggedin";
 import SearchResultsPage from "./Components/SearchResultsPage";
-import UserGetin from "./Components/UserGetin";
 import Upload from "./Components/Upload";
 import { UserContext } from "./UserContext";
+import DataContext from "./DataContext";
 
 function App() {
   const [value, setValue] = useState("user");
   const providerValue = useMemo(() => ({ value, setValue }), [value, setValue]);
+  const [contextArray, setContextArray] = useState(["H"]);
+
   return (
     <Router>
+      <Route path="/login" component={Login} />
+      <Route path="/register" component={Register} />
       <UserContext.Provider value={providerValue}>
-        <Route path="/login" component={Login} />
-        <Route path="/register" component={Register} />
-        <Route path="/loggedin" component={Loggedin} />
-        <Route path="/searchpage" component={Searchpage} />
-        <Route path="/search" component={SearchResultsPage} />
-        <Route exact path="/" component={UserGetin} />
+        <DataContext.Provider value={{ contextArray, setContextArray }}>
+          <Route path="/searchpage" component={Searchpage} />
+          <Route path="/search" component={SearchResultsPage} />
+        </DataContext.Provider>
         <Route path="/upload" component={Upload} />
       </UserContext.Provider>
     </Router>
