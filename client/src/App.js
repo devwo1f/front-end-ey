185cdab7cfffb6ac226eb8b388d2d66b0f092331
devwo1f <48999616+devwo1f@users.noreply.github.com>
2021-10-12 14:10:58 +0530

    FC

--- client/src/App.js
+++ client/src/App.js
@@ -6,19 +6,14 @@ import Searchpage from "./Components/Searchpage";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import SearchResultsPage from "./Components/SearchResultsPage";
 import Upload from "./Components/Upload";
-import DataContext from "./DataContext";
 
 function App() {
-  const [contextArray, setContextArray] = useState(["H"]);
-
   return (
     <Router>
       <Route path="/login" component={Login} />
       <Route path="/register" component={Register} />
-      <DataContext.Provider value={{ contextArray, setContextArray }}>
-        <Route path="/searchpage" component={Searchpage} />
-        <Route path="/search" component={SearchResultsPage} />
-      </DataContext.Provider>
+      <Route path="/searchpage" component={Searchpage} />
+      <Route path="/search" component={SearchResultsPage} />
       <Route path="/upload" component={Upload} />
     </Router>
   );
